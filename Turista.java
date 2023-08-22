fafccd8d604ac0f01a2ddfffff0e2b22f6a0eb99
Juanma <juanmartin.pilon@gmail.com>
2023-08-22 10:44:09 -0300

    Implementando algunas clases del Diagrama

---
+++ Turista.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Turista extends Usuario {
+    private int nacionalidad;
+    private List <Compra> compras = new ArrayList<>();
+
+    public Turista (String nickname, String nombre, String apellido, String correo, Date fNacimiento) {
+        super(nickname, nombre, apellido, correo, fNacimiento);
+       this.nacionalidad = nacionalidad;
+    }
+
+    public void setNacionalidad(int nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public int getNacionalidad() {
+        return nacionalidad;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
