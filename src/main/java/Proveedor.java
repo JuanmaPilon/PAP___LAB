cc354eeec1ceb8ce8032a8113f73403842094024
Juanma <juanmartin.pilon@gmail.com>
2023-08-29 12:00:51 -0300

    Procurando que esta porqueria ande bien

---
+++ src/main/java/Proveedor.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class Proveedor extends Usuario {
+    private String descripcion;
+    private String link;
+
+    public Proveedor(String nickname, String nombre, String apellido, String correo, Date fNacimiento) {
+        super(nickname, nombre, apellido, correo, fNacimiento);
+        this.descripcion = descripcion;
+        this.link = link;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
