eed6f567a6db2ae0ff45f1ce9ea28b620af3f222
Carlangassss <carlossantana5017@gmail.com>
2023-10-02 21:48:08 -0300

    capas de logica y persistencia agregadas sin errores (creo)

---
+++ Web Server/src/main/java/logica/Compra.java
@@ -0,0 +1,51 @@
+package logica;
+
+
+
+import java.util.Date;
+
+public class Compra {
+    private Date fCompra;
+    private int fContTurista;
+    private float fCostoTotal;
+    private Date vencimiento;
+    private Paquete paquete;
+    public Compra(Date fCompra, int fContTurista, float fCostoTotal, Date vencimiento) {
+        this.fCompra = fCompra;
+        this.fContTurista = fContTurista;
+        this.fCostoTotal = fCostoTotal;
+        this.vencimiento = vencimiento;
+        this.paquete = paquete;
+    }
+    public void setPaquete(Paquete paquete) {
+        this.paquete = paquete;
+    }
+    public Paquete getPaquete() {
+        return paquete;
+    }
+    public Date getfCompra() {
+        return fCompra;
+    }
+    public int getfContTurista() {
+        return fContTurista;
+    }
+    public float getfCostoTotal() {
+        return fCostoTotal;
+    }
+    public Date getVencimiento() {
+        return vencimiento;
+    }
+    public void setfCompra(Date fCompra) {
+        this.fCompra = fCompra;
+    }
+    public void setfContTurista(int fContTurista) {
+        this.fContTurista = fContTurista;
+    }
+    public void setfCostoTotal(float fCostoTotal) {
+        this.fCostoTotal = fCostoTotal;
+    }
+    public void setVencimiento(Date vencimiento) {
+        this.vencimiento = vencimiento;
+    }
+
+}
